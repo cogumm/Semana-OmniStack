9e10937d66a42fc8f52123f4bc34df04cc645b56
Gabriel F. Vilar <cogumm@gmail.com>
2020-02-12 13:20:08 -0300

    9semana: Criando e iniciandi o projeto backend

---
+++ 9semana/backend/src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const db = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+// GET, POST, PUT, DELETE
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+// Conexão com o banco de dados
+db.connect(
+  "mongodb+srv://semana:semana@mrmeeseeks-2yyps.mongodb.net/SemanaOmniStack9?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3001, () => {
+  console.log("Servidor backend inicializado com sucesso na porta 3001");
+});
