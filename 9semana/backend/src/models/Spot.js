ed207acfbfd9a5ffea3ba5dd706ab45920f6c601
Gabriel F. Vilar <cogumm@gmail.com>
2020-02-14 15:09:31 -0300

    9semana: backend adicionado campo virtual

--- 9semana/backend/src/models/Spot.js
+++ 9semana/backend/src/models/Spot.js
@@ -1,14 +1,26 @@
 const db = require("mongoose");
 
-const SpotSchema = new db.Schema({
-  thumbnail: String,
-  company: String,
-  price: Number,
-  techs: [String],
-  user: {
-    type: db.Schema.Types.ObjectId,
-    ref: "User"
+const SpotSchema = new db.Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+      type: db.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+
+// Campo virtual
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:3001/files/${this.thumbnail}`;
 });
 
 module.exports = db.model("Spot", SpotSchema);
--- 9semana/backend/src/server.js
+++ 9semana/backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -21,6 +22,7 @@ db.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3001, () => {
